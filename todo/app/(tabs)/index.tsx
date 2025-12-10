5dbb2d12e48c0a6336b5ae6e961a0b6845f17503
SagarKumar977 <sag197348@gmail.com>
2025-12-10 21:32:19 +0530

    update

--- todo/app/(tabs)/index.tsx
+++ todo/app/(tabs)/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import { useTheme } from "@/hook/useTheme";
+import { StatusBar, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+
 
 export default function Index() {
+  const { toggleDarkMode, colors } = useTheme()
+
+  const styles = createHomeStyles(colors);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <StatusBar hidden={true} />
+      <SafeAreaView
+        style={styles.safeArea}
+      >
+        <View
+          style={styles.container}
+        >
+          <Text>Edit app/index.tsx to edit this screen.</Text>
+          <TouchableOpacity onPress={toggleDarkMode}>
+            <Text>Toggle Background</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
---
+++ todo/convex/todos.ts
@@ -0,0 +1,70 @@
+import { query, mutation } from './_generated/server';
+import { ConvexError,v } from 'convex/values';
+
+
+export const getTodos = query({
+    handler: async (ctx) => {
+        const todos = await ctx.db.query("todos").order("desc").collect();
+        return todos;
+    }
+});
+
+export const addTodos = mutation({
+    args:{
+        text:v.string()
+    },
+    handler: async (ctx,args)=>{
+        const todosId = await ctx.db.insert("todos",{
+            text:args.text,
+            isCompleted:false
+        })
+        return todosId;
+    }
+});
+
+export const toggleTodos = mutation({
+    args:{
+        id:v.id("todos")
+    },
+    handler:async (ctx,args)=>{
+        const todos = await ctx.db.get(args.id);
+        if(!todos) throw new ConvexError("Todos not found");
+
+        await ctx.db.patch(args.id,{
+            isCompleted:!todos.isCompleted
+        })
+    }
+})
+
+export const updateTodos = mutation({
+    args:{
+        id:v.id("todos"),
+        text:v.string()
+    },
+    handler:async (ctx,args)=>{
+        const todos = await ctx.db.get(args.id);
+        if(!todos) throw new ConvexError("Todos not found");
+
+        await ctx.db.patch(args.id,{
+            text:args.text
+        })
+    }
+})
+
+export const deleteTodos = mutation({
+    args:{
+        id:v.id("todos")
+    },
+    handler:async (ctx,args)=>{
+        await ctx.db.delete(args.id)
+    }
+})
+
+export const clearAllTodos = mutation({
+    handler:async (ctx,args)=>{
+        const todos = await ctx.db.query("todos").collect();
+        for(const todo of todos){
+            await ctx.db.delete(todo._id)
+        }
+    }
+})
